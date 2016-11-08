60a692c60e4733e40005c22b8e3b8688ba4c2f9b
chhit5249 <chhit5249@7JGN6Y1.Student.UGDSB.ED>
2016-11-08 13:58:21 -0500

    Version 0.2: Functional. Good commenting/documentation. User can now input the amount of #s.

--- src/evensandodds/EvensAndOdds.java
+++ src/evensandodds/EvensAndOdds.java
@@ -1,24 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package evensandodds;
+import java.util.*;
 
 /**
- *
+ * Name: Chris Hitchcock
+ * Date: November 8th, 2016
+ * Version: 0.2
+ * Description: This code tests the List class
  * @author chhit5249
  */
 public class EvensAndOdds {
 
     /**
+     * Everything happens here
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        List test = new List(25);
-        System.out.println(test.evenOdds());
+        //Variable/object declaration
+        int l;
+        Scanner kb = new Scanner(System.in);
+        
+        //Prompt for length and set length
+        System.out.println("How many numbers do you wish to generate?");
+        l = kb.nextInt();
+        
+        //Create the List and print out the even/odd lists
+        List nums = new List(l);
+        System.out.println(nums.evenOdds());
     }
-    
+
 }
--- src/evensandodds/List.java
+++ src/evensandodds/List.java
@@ -1,49 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package evensandodds;
+
 import java.util.*;
+
 /**
- *
+ * Name: Chris Hitchcock
+ * Date: November 8th 2016
+ * Version: 0.2
+ * Description: This class creates a list of randomly generated numbers, and 
+ * contains a method that sorts them into evens and odds for output.
  * @author chhit5249
  */
 public class List {
+    //Variable/object declaration
     private int[] numbers;
     private ArrayList<Integer> evens, odds;
     Random r = new Random();
-    public List(int length)
-    {
+
+    /**
+     * Constructor <p>
+     * pre: length passed <br>
+     * post: List created
+     * @param length 
+     */
+    public List(int length) {
+        //Create an array, generate random numbers, and add those numbers to the array
         numbers = new int[length];
-        
-        for (int i = 0;i<=numbers.length-1;i++)
-        {
+        for (int i = 0; i <= numbers.length - 1; i++) {
             int rand = r.nextInt(99);
-            numbers[i]=rand;
+            numbers[i] = rand;
         }
     }
-    
-    public String evenOdds()
-    {
-       String output = "ph";
-       evens = new ArrayList();
-       odds = new ArrayList();
-       for (int x = 0;x <= numbers.length-1; x++)
-       {
-           int check;
-           check = numbers[x]%2;
-           if (check==0)
-           {
-               evens.add(numbers[x]);
-           } else {
-               odds.add(numbers[x]);
-           }
-       }
-       //Collections.sort(odds);
-       //Collections.sort(evens);
-       output = "EVENS:\n"+evens+"\nODDS:\n"+odds;
-       return output;
+
+    /**
+     * This method organizes the random numbers into even numbers and odd numbers
+     * using modulus division <p>
+     * pre: none <br>
+     * post: numbers added to even/odd ArrayLists, and added to output string for return
+     * @return output - output string that displays the even and odd lists.
+     */
+    public String evenOdds() {
+        //Variabe/object declaration
+        String output;
+        evens = new ArrayList();
+        odds = new ArrayList();
+        
+        //For loop which goes through each number in the numbers array and adds them to the correct ArrayList
+        for (int x = 0; x <= numbers.length - 1; x++) {
+            int check;
+            check = numbers[x] % 2;
+            if (check == 0) {
+                evens.add(numbers[x]);
+            } else {
+                odds.add(numbers[x]);
+            }
+        }
+        
+        //Add the elements from evens/odds ArrayLists to output using for-each loops
+        output = "EVENS: \n";
+        for (Object nums : evens) {
+            output+=nums+" ";
+        }
+        output += "\nODDS: \n";
+        for (Object nums : odds) {
+            output+=nums+" ";
+        }
+        return output;
     }
 }
